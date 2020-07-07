8b581d3432357bb01869b646731b130a1277978a
megan <meggsnbacon777@gmail.com>
2020-07-07 01:54:58 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -2,11 +2,11 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        name: "Lord Montague",age:"53"
+        name: "Lord Montague",age: "53"
         #your key/value pairs here
         },
       :matriarch => {
-      name: "Lord Montague",age:"54"  #your key/value pairs here
+      name: "Lord Montague",age: "54"  #your key/value pairs here
         },
       :hero => {
        name:"Romeo",age: "15",status:"alive" #your key/value pairs here
