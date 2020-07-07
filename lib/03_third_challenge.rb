b6822963c611fe5b2de4470a69256c2aee7a88cc
megan <meggsnbacon777@gmail.com>
2020-07-07 02:01:29 +0000

    Done.

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -2,24 +2,27 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
+        name: "Lord Montague",age: "53"
         #your key/value pairs here
         },
       :matriarch => {
-        #your key/value pairs here
+      name: "Lady Montague",age: "54"  #your key/value pairs here
         },
       :hero => {
-        #your key/value pairs here
+       name:"Romeo",age: "15",status:"alive" #your key/value pairs here
         },
-      :hero_friends => []
+      :hero_friends =>[]
    }, 
    :capulet => {
       :patriarch => {
-        #your key/value pairs here
+        name: "Lord Capulet",age:"50"#your key/value pairs here
         },
       :matriarch => {
+        name: "Lady Capulet",age:"51"
         #your key/value pairs here
         },
       :heroine => {
+        name: "Juliet",age: "15",status:"alive"
         #your key/value pairs here
         },
       :heroine_friends => []
