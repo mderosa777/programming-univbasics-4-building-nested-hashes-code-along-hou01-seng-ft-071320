1638538d217c26a2d0986f9e6e3650cdbc80d570
megan <meggsnbacon777@gmail.com>
2020-07-07 02:01:17 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -6,7 +6,7 @@ def third_challenge
         #your key/value pairs here
         },
       :matriarch => {
-      name: "Lord Montague",age: "54"  #your key/value pairs here
+      name: "Lady Montague",age: "54"  #your key/value pairs here
         },
       :hero => {
        name:"Romeo",age: "15",status:"alive" #your key/value pairs here
